d1cb5a0ab91bb4ff2d193338559842201bbc854c
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2021-04-06 12:23:29 -0600

    Question options

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,9 @@ var questions = [
         answer: "//comment here"
       },
 ]
+var questionIndex = 0;  // question indexes that may keep track quiz state and time 
+var time = questions.length * 15;
+var timerId;
 // Golbal variable that connect our js functions with our Html classes & Ids
 var questionsElement = document.getElementById("questions");
 var timerElement = document.getElementById("time");
@@ -35,13 +38,45 @@ var submitBtn = document.getElementById("submit");
 var startBtnElement = document.getElementById("start");
 var initialsElement = document.getElementById("initials");
 var feedbackElement = document.getElementById("feedback");
-var questionIndex = 0;  // question indexes that may keep track quiz state and time 
-var time = questions.length * 15;
-var timerId;
+
 // Start function that hides start screen, removes hide class from functions and starts timer
+function timeIncement(){
+    time = time -1
+    timerElement.textContent = time
+    if(time <= 0){console.log("Quiz is over")}
+}
+function QuizStart(){
+    var startScreen = document.getElementById("start-screen")
+    startScreen.setAttribute("class","hide")
+    questionsElement.removeAttribute("class")
+    timerId = setInterval(timeIncement,1000)
+    timerElement.textContent = time
+    // Grab question function 
+    GrabQuestion()
 
+}
 // function that displays questions and creates buttons based on question answers
-
+ function GrabQuestion(){
+     //  question object from array
+  var currentQuestion = questions[questionIndex];
+ 
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  // clear out any old question choices
+  choicesElement.innerHTML = "";
+  // loop over choices
+  currentQuestion.options.forEach(function(option, i) {
+    // create new button for each choice
+    var choiceButton= document.createElement("button");
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.setAttribute("value", option);
+    choiceButton.textContent = i + 1 + ". " + option;
+    // attach click event listener to each choice
+    // choiceButton.onclick = questionClick;
+    // display on the page
+    choicesElement.appendChild(choiceButton);
+  });
+ }   
 // function to check if answer is correct or not
 
 // Quiz end function that stops timer with a clear interval, hides question screen, removes hide from end screen 
@@ -49,3 +84,4 @@ var timerId;
 // function to save high scores 
 
 // click events that link buttons with above functions 
+startBtnElement.onclick = QuizStart
