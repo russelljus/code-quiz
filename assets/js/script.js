f5958560055927b4d1c5bdff97dee78362bce78b
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2021-04-06 11:26:17 -0600

    notes in js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Add question array here
+
+// Golbal variable that connect our js functions with our Html classes & Ids
+
+// Start function that hides start screen, removes hide class from functions and starts timer
+
+// function that displays questions and creates buttons based on question answers
+
+// function to check if answer is correct or not
+
+// Quiz end function that stops timer with a clear interval, hides question screen, removes hide from end screen 
+
+// function to save high scores 
+
+// click events that link buttons with above functions 
